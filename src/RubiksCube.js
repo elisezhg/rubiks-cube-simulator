243d22e794e2a8d83ea7c741fb22d2e649e0b7d6
_elise_ <elise.zheng@umontreal.ca>
2021-08-21 20:30:14 -0400

    Mini cubes color mapping

--- src/RubiksCube.js
+++ src/RubiksCube.js
@@ -1,13 +1,232 @@
 class RubiksCube {
     constructor() {
         this.state = [
-            Array(9).fill(0x00ff00), // Green
             Array(9).fill(0xffffff), // White
+            Array(9).fill(0x00ff00), // Green
             Array(9).fill(0xff0000), // Red
-            Array(9).fill(0x00ffff), // yellow
-            Array(9).fill(0xffff00), // Orange
-            Array(9).fill(0x0000ff)  // Blue
+            Array(9).fill(0x0000ff), // Blue
+            Array(9).fill(0xffa500), // Orange
+            Array(9).fill(0xffff00)  // Yellow
         ];
+
+        this.miniCubesMapping = {
+            0: {
+                0: this.state[0][0],
+                1: null,
+                2: null,
+                3: this.state[3][2],
+                4: this.state[4][0],
+                5: null
+            },
+            1: {
+                0: this.state[0][1],
+                1: null,
+                2: null,
+                3: this.state[3][1],
+                4: null,
+                5: null
+            },
+            2: {
+                0: this.state[0][2],
+                1: null,
+                2: this.state[2][2],
+                3: this.state[3][3],
+                4: null,
+                5: null
+            },
+            3: {
+                0: null,
+                1: null,
+                2: null,
+                3: this.state[3][5],
+                4: this.state[4][3],
+                5: null
+            },
+            4: {
+                0: null,
+                1: null,
+                2: null,
+                3: this.state[3][4],
+                4: null,
+                5: null
+            },
+            5: {
+                0: null,
+                1: null,
+                2: this.state[2][5],
+                3: this.state[3][3],
+                4: null,
+                5: null
+            },
+            6: {
+                0: null,
+                1: null,
+                2: null,
+                3: this.state[3][8],
+                4: this.state[4][6],
+                5: this.state[5][6]
+            },
+            7: {
+                0: null,
+                1: null,
+                2: null,
+                3: this.state[3][7],
+                4: null,
+                5: this.state[5][7]
+            },
+            8: {
+                0: null,
+                1: null,
+                2: this.state[2][8],
+                3: this.state[3][6],
+                4: null,
+                5: this.state[5][6]
+            },
+            9: {
+                0: this.state[0][3],
+                1: null,
+                2: null,
+                3: null,
+                4: this.state[4][1],
+                5: null
+            },
+            10: {
+                0: this.state[0][4],
+                1: null,
+                2: null,
+                3: null,
+                4: null,
+                5: null
+            },
+            11: {
+                0: this.state[0][2],
+                1: null,
+                2: this.state[2][1],
+                3: null,
+                4: null,
+                5: null
+            },
+            12: {
+                0: null,
+                1: null,
+                2: null,
+                3: null,
+                4: this.state[4][4],
+                5: null
+            },
+            13: {
+                0: null,
+                1: null,
+                2: null,
+                3: null,
+                4: null,
+                5: null
+            },
+            14: {
+                0: null,
+                1: null,
+                2: this.state[2][4],
+                3: null,
+                4: null,
+                5: null
+            },
+            15: {
+                0: null,
+                1: null,
+                2: null,
+                3: null,
+                4: this.state[4][7],
+                5: this.state[5][3]
+            },
+            16: {
+                0: null,
+                1: null,
+                2: null,
+                3: null,
+                4: null,
+                5: this.state[5][4]
+            },
+            17: {
+                0: null,
+                1: null,
+                2: this.state[2][7],
+                3: null,
+                4: null,
+                5: this.state[5][5]
+            },
+            18: {
+                0: this.state[0][6],
+                1: this.state[1][0],
+                2: null,
+                3: null,
+                4: this.state[4][3],
+                5: null
+            },
+            19: {
+                0: this.state[0][7],
+                1: this.state[1][1],
+                2: null,
+                3: null,
+                4: null,
+                5: null
+            },
+            20: {
+                0: this.state[0][2],
+                1: this.state[1][2],
+                2: this.state[2][0],
+                3: null,
+                4: null,
+                5: null
+            },
+            21: {
+                0: null,
+                1: this.state[1][3],
+                2: null,
+                3: null,
+                4: this.state[4][1],
+                5: null
+            },
+            22: {
+                0: null,
+                1: this.state[1][4],
+                2: null,
+                3: null,
+                4: null,
+                5: null
+            },
+            23: {
+                0: null,
+                1: this.state[1][5],
+                2: this.state[2][3],
+                3: null,
+                4: null,
+                5: null
+            },
+            24: {
+                0: null,
+                1: this.state[1][6],
+                2: null,
+                3: null,
+                4: this.state[4][8],
+                5: this.state[5][0]
+            },
+            25: {
+                0: null,
+                1: this.state[1][7],
+                2: null,
+                3: null,
+                4: null,
+                5: this.state[5][0]
+            },
+            26: {
+                0: null,
+                1: this.state[1][8],
+                2: this.state[2][6],
+                3: null,
+                4: null,
+                5: this.state[5][2]
+            }
+        }
 
         this.size = 3;
     }
@@ -21,7 +240,13 @@ class RubiksCube {
     }
 
     getColor(x, y, z) {
-        // TODO
+        let index = x + 3 * (2-y) + 9 * z;
+        console.log(`x: ${x}, y: ${y}, z: ${z} -> ${index} ${this.miniCubesMapping[index]}`);
+        return this.miniCubesMapping[index];
+    }
+
+    getColorUpdated(i) {
+        return this.miniCubesMapping[i];
     }
 
     turnFrontClockwise() {
